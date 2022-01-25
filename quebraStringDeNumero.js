9d9bff4c8d4d6a503d06088242a38e65b84ea229
brutar <bruno.tar.gz@gmail.com>
2022-01-24 22:11:36 -0300

    Adiciona arquivo sgnore

--- quebraStringDeNumero.js
+++ quebraStringDeNumero.js
@@ -1,8 +1,13 @@
+//Quebra em 2 caracteres uma string que contém uma sequência numérica
 let numerozao = "1202031424545222231324354131116151224253181515151515151515151515151515151515151515151230";
 let num = '';
 let numerosQuebrados = [];
+
+//percorre cada posição da string
 for(let i = 0; i < numerozao.length;i++){
     num = num + numerozao[i];
+    //adiciona uma sequencia de 2 caracteres em no array numerosQuebrados
+    //no caso de sobrar 1 carater, é adicionado 0 na frente desse caracter
     if(i%2 == 1){
       numerosQuebrados.push(num);
       num = '';
